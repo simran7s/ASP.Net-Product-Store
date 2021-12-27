a5e26f87c7fd0e56eaafd30560924a1a0bcdfa50
simran7s <simran7s@my.yorku.ca>
2021-12-27 12:46:23 -0500

    displayed products on site and added styling

--- ContosoCrafts.WebSite/Pages/Index.cshtml.cs
+++ ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -5,21 +5,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //Create a set of Products that are private to this class
+        public IEnumerable<Product> Products { get; private set; }
+
+
+        //added JSON file Product service
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
+
+        //When someone "GETS" this page. What should we do?
         public void OnGet()
         {
-
+            //define Products to the Products returned by the ProductService (JsonFileProductService)
+            Products = ProductService.GetProducts();
         }
     }
 }
